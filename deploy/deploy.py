f37bc1a35ef54fce68477b5093b0c568e126c571
Austen Tulodziecki <ezio66@hotmail.co.uk>
2021-11-04 11:54:46 +0000

    try again again again

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -4,7 +4,7 @@ import sys, getopt
 
 def deploy():
     with py7zr.SevenZipFile(f'release.{sys.argv[1]}.7z', mode='w') as z:
-        z.write(f"{os.path.dirname(__file__)}..\\..\\soil-moisture-sensor\\app.py", "app.py")
+        z.write(f"{os.path.dirname(__file__)}../../soil-moisture-sensor/app.py", "app.py")
         z.write("iot_config.json")
         z.write("readme.md")
 
